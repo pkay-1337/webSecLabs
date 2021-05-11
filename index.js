76403824f5492cb6a2ec6dde8e9900ccc112d6af
pramod <kumarpramod66476@gmail.com>
2021-05-11 00:56:53 -0700

    Basic Authentication

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let express = require('express');
+const { waitForDebugger } = require('inspector');
 const path = require('path')
 let app = new express();
 
@@ -8,4 +9,27 @@ app.get('/',(req,res)=>{
 app.get('/auth',(req,res)=>{
     res.send('Work in progress')
 })
-app.listen(8080)
+app.get('/secret',(req,res)=>{
+    if(!req.headers.authorization){
+        res.set({
+            'WWW-Authenticate':'Basic Realm="Who are You?"'
+        })
+        res.status(401)
+        res.end()
+    }
+    console.log(req.headers.authorization)
+    if(req.headers.authorization === 'Basic amh1cmk6IkBqaHVyaSI='){
+        res.set({
+            "Content-type":"text/html;"
+        })
+        res.sendFile(path.resolve(process.cwd(),'secret'))
+    }else{
+        res.set({
+            'WWW-Authenticate':'Basic Realm="Who are You?"'
+        })
+        res.status(401)
+        res.end()
+    }
+    
+})
+app.listen(80)
