57c4618b68e0b20be86188ecbc15787c732f18a6
pramod <kumarpramod66476@gmail.com>
2021-05-10 22:01:22 -0700

    some things working

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = new express()
+let express = require('express');
+const path = require('path')
+let app = new express();
 
 app.get('/',(req,res)=>{
-    res.send('Test')
+    res.sendFile(path.resolve(process.cwd(),'index.html'))
 })
+app.get('/auth',(req,res)=>{
+    res.send('Work in progress')
+})
+app.listen(8080)
